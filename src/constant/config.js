db0fa0c2059b1227d339c37835ce4e804361ee43
thenaursa <thenaursa@gmail.com>
2024-02-28 18:00:24 +0100

    feat: banner update

--- src/constant/config.js
+++ src/constant/config.js
@@ -2,5 +2,5 @@ export const siteConfig = {
   title: 'THENA',
   description:
     'Trade your favourite tokens with low-slippage and efficient pricing. Provide liquidity to earn yields, get paid to vote for pools and more!',
-  url: 'https://www.thena.fi',
+  url: 'https://thena.fi',
 }
