9ec0f34e702b6d9f132ba2ed6e9cef31a37c0c58
thenaursa <thenaursa@gmail.com>
2024-03-18 16:59:01 +0100

    feat: revert particle

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { AuthCoreContextProvider } from '@particle-network/auth-core-modal'
 import { Provider } from 'react-redux'
 import { ToastContainer, Zoom } from 'react-toastify'
 import { SWRConfig } from 'swr'
@@ -38,68 +37,68 @@ function ContextProviders({ children }) {
 
 export function Providers({ children }) {
   return (
-    <AuthCoreContextProvider
-      options={{
-        projectId: process.env.NEXT_PUBLIC_PARTICLE_PROJECT_ID,
-        clientKey: process.env.NEXT_PUBLIC_PARTICLE_CLIENT_KEY,
-        appId: process.env.NEXT_PUBLIC_PARTICLE_APP_ID,
-        themeType: 'dark',
-        customStyle: {
-          zIndex: 2147483650, // must greater than 2147483646
-          logo: 'https://cdn.thena.fi/assets/THE.png',
-          projectName: 'THENA',
-          primaryBtnBorderRadius: '8px',
-          modalBorderRadius: '12px',
-          cardBorderRadius: '12px',
-          fontFamily: 'Inter',
-          theme: {
-            dark: {
-              primaryBtnColor: '#FCE6FB',
-              primaryBtnBackgroundColor: '#DC00D4',
-              secondaryBtnColor: '#ECEAED',
-              secondaryBtnBackgroundColor: '#35243D',
-              textColor: '#F3F2F4',
-              secondaryTextColor: '#B3ABB7',
-              themeBackgroundColor: '#1A121E',
-              iconBorderColor: '#35243D',
-              accentColor: '#DC00D4',
-              inputBackgroundColor: '#35243D',
-              inputBorderColor: '#685770',
-              inputPlaceholderColor: '#8E8194',
-              cardBorderColor: '#35243D',
-              cardUnclickableBackgroundColor: 'none',
-              cardUnclickableBorderColor: '#35243D',
-              cardDividerColor: '#35243D',
-            },
-          },
-        },
-        wallet: {
-          visible: false,
-        },
-      }}
-    >
-      <Provider store={store}>
-        <SWRConfig
-          value={{
-            refreshInterval: 30000,
-            refreshWhenHidden: false,
-            refreshWhenOffline: false,
-            use: [swrGCMiddleware],
-          }}
-        >
-          <ToastContainer
-            className='notify-class'
-            position='bottom-left'
-            theme='dark'
-            closeOnClick={false}
-            transition={Zoom}
-            autoClose={5000}
-            hideProgressBar
-            closeButton={false}
-          />
-          <ContextProviders>{children}</ContextProviders>
-        </SWRConfig>
-      </Provider>
-    </AuthCoreContextProvider>
+    // <AuthCoreContextProvider
+    //   options={{
+    //     projectId: process.env.NEXT_PUBLIC_PARTICLE_PROJECT_ID,
+    //     clientKey: process.env.NEXT_PUBLIC_PARTICLE_CLIENT_KEY,
+    //     appId: process.env.NEXT_PUBLIC_PARTICLE_APP_ID,
+    //     themeType: 'dark',
+    //     customStyle: {
+    //       zIndex: 2147483650, // must greater than 2147483646
+    //       logo: 'https://cdn.thena.fi/assets/THE.png',
+    //       projectName: 'THENA',
+    //       primaryBtnBorderRadius: '8px',
+    //       modalBorderRadius: '12px',
+    //       cardBorderRadius: '12px',
+    //       fontFamily: 'Inter',
+    //       theme: {
+    //         dark: {
+    //           primaryBtnColor: '#FCE6FB',
+    //           primaryBtnBackgroundColor: '#DC00D4',
+    //           secondaryBtnColor: '#ECEAED',
+    //           secondaryBtnBackgroundColor: '#35243D',
+    //           textColor: '#F3F2F4',
+    //           secondaryTextColor: '#B3ABB7',
+    //           themeBackgroundColor: '#1A121E',
+    //           iconBorderColor: '#35243D',
+    //           accentColor: '#DC00D4',
+    //           inputBackgroundColor: '#35243D',
+    //           inputBorderColor: '#685770',
+    //           inputPlaceholderColor: '#8E8194',
+    //           cardBorderColor: '#35243D',
+    //           cardUnclickableBackgroundColor: 'none',
+    //           cardUnclickableBorderColor: '#35243D',
+    //           cardDividerColor: '#35243D',
+    //         },
+    //       },
+    //     },
+    //     wallet: {
+    //       visible: false,
+    //     },
+    //   }}
+    // >
+    <Provider store={store}>
+      <SWRConfig
+        value={{
+          refreshInterval: 30000,
+          refreshWhenHidden: false,
+          refreshWhenOffline: false,
+          use: [swrGCMiddleware],
+        }}
+      >
+        <ToastContainer
+          className='notify-class'
+          position='bottom-left'
+          theme='dark'
+          closeOnClick={false}
+          transition={Zoom}
+          autoClose={5000}
+          hideProgressBar
+          closeButton={false}
+        />
+        <ContextProviders>{children}</ContextProviders>
+      </SWRConfig>
+    </Provider>
+    // </AuthCoreContextProvider>
   )
 }
