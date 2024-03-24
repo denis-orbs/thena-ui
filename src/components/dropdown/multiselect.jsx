f26d5be23fa2555a03045e6297f984a1d4daa953
thenaursa <thenaursa@gmail.com>
2024-03-24 21:25:54 +0100

    feat: thenft intl

--- src/components/dropdown/multiselect.jsx
+++ src/components/dropdown/multiselect.jsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useTranslations } from 'next-intl'
 import React, { useEffect, useRef, useState } from 'react'
 
 import { cn } from '@/lib/utils'
@@ -11,6 +12,7 @@ import Input from '../input'
 function MultiSelect({ className, data, selected, setSelected, placeHolder }) {
   const [open, setOpen] = useState(false)
   const wrapperRef = useRef(null)
+  const t = useTranslations()
 
   const handleCheckBox = item => {
     const temp = [...selected]
@@ -43,7 +45,7 @@ function MultiSelect({ className, data, selected, setSelected, placeHolder }) {
         type='text'
         val={selected}
         onClick={() => setOpen(!open)}
-        placeholder={placeHolder}
+        placeholder={t(placeHolder)}
         TrailingIcon={
           <ChevronDownIcon
             className={cn('transfrom transition-all duration-150 ease-out', open ? 'rotate-180' : 'rotate-0')}
