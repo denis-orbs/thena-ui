c4ecf131f77ad8387a1f6f71162eaad93885796c
thenaursa <thenaursa@gmail.com>
2024-03-25 16:45:14 +0100

    feat: particle add

--- src/context/Web3Modal.jsx
+++ src/context/Web3Modal.jsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { AuthCoreContextProvider } from '@particle-network/auth-core-modal'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createWeb3Modal } from '@web3modal/wagmi/react'
 import { ChainId } from 'thena-sdk-core/dist'
@@ -46,7 +47,7 @@ export const wagmiConfig = createConfig({
 })
 
 // 3. Create modal
-createWeb3Modal({
+const web3Modal = createWeb3Modal({
   wagmiConfig,
   projectId,
   chains,
@@ -56,10 +57,100 @@ createWeb3Modal({
   },
 })
 
+let connecteFlag = 1
+
+web3Modal.wagmiConfig.subscribe(state => {
+  if (typeof window === 'undefined') return
+  if (state.status === 'connected') {
+    connecteFlag = 1
+  }
+  const particle = window?.particle
+  if (state.status === 'disconnected' && particle && particle.ethereum.isConnected() && connecteFlag !== 0) {
+    const config = web3Modal?.wagmiConfig
+    const connector = config.connectors.find(item => item.id === 'network.particle')
+    if (connector) {
+      connector.connect({ isReconnecting: true }).then(data => {
+        const connections = new Map(new Map()).set(connector.uid, {
+          accounts: data.accounts,
+          chainId: data.chainId,
+          connector,
+        })
+        const stateData = {
+          ...config.state,
+          current: connector.uid,
+          connections,
+          status: 'connected',
+        }
+        web3Modal.wagmiConfig.setState(stateData)
+      })
+    }
+  }
+})
+
+web3Modal.subscribeEvents(event => {
+  if (typeof window === 'undefined') return
+  const particle = window?.particle
+  if (
+    particle &&
+    event.data.event === 'MODAL_CLOSE' &&
+    web3Modal.wagmiConfig.state.status === 'disconnected' &&
+    particle.ethereum.isConnected()
+  ) {
+    connecteFlag = 0
+    particle.ethereum.disconnect().then(() => {
+      web3Modal.wagmiConfig.setState({
+        chainId: ChainId.BSC,
+        connections: {},
+        status: 'disconnected',
+      })
+    })
+  }
+})
+
 export function Web3Modal({ children, initialState }) {
   return (
-    <WagmiProvider config={wagmiConfig} initialState={initialState}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </WagmiProvider>
+    <AuthCoreContextProvider
+      options={{
+        projectId: process.env.NEXT_PUBLIC_PARTICLE_PROJECT_ID,
+        clientKey: process.env.NEXT_PUBLIC_PARTICLE_CLIENT_KEY,
+        appId: process.env.NEXT_PUBLIC_PARTICLE_APP_ID,
+        themeType: 'dark',
+        customStyle: {
+          logo: 'https://cdn.thena.fi/assets/THE.png',
+          projectName: 'THENA',
+          primaryBtnBorderRadius: '8px',
+          modalBorderRadius: '12px',
+          cardBorderRadius: '12px',
+          fontFamily: 'Inter',
+          theme: {
+            dark: {
+              primaryBtnColor: '#FCE6FB',
+              primaryBtnBackgroundColor: '#DC00D4',
+              secondaryBtnColor: '#ECEAED',
+              secondaryBtnBackgroundColor: '#35243D',
+              textColor: '#F3F2F4',
+              secondaryTextColor: '#B3ABB7',
+              themeBackgroundColor: '#1A121E',
+              iconBorderColor: '#35243D',
+              accentColor: '#DC00D4',
+              inputBackgroundColor: '#35243D',
+              inputBorderColor: '#685770',
+              inputPlaceholderColor: '#8E8194',
+              cardBorderColor: '#35243D',
+              cardUnclickableBackgroundColor: 'none',
+              cardUnclickableBorderColor: '#35243D',
+              cardDividerColor: '#35243D',
+            },
+          },
+        },
+        wallet: {
+          visible: false,
+        },
+      }}
+    >
+      <WagmiProvider config={wagmiConfig} initialState={initialState}>
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      </WagmiProvider>
+    </AuthCoreContextProvider>
   )
 }
