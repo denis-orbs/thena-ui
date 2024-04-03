b6591a013697fe232265f926861bcd10121c60b1
namnh <nam.nh1@zinza.com.vn>
2024-04-03 11:16:37 +0700

    feat: arena page update side bar

--- src/app/arena/trading-competitions/[id]/SideBar.jsx
+++ src/app/arena/trading-competitions/[id]/SideBar.jsx
@@ -42,10 +42,133 @@ function Sidebar({ competition }) {
     [competition.timestamp.registrationEnd],
   )
 
+  const isClaimed = false
+
+  const headingAndText = useMemo(() => {
+    if (isInRegistration) {
+      return {
+        heading: t('Registration'),
+        text: isJoined ? t('Competition Joined') : null,
+        subText: isJoined ? t('You Joined This Competition') : null,
+      }
+    }
+    if (eventType === EVENT_TYPES.LIVE) {
+      let subText = null
+      switch (competition.participantCount) {
+        case 0:
+          subText = t('No Thenians Are Competing', {
+            totalPrize: formatAmount(fromWei(competition.prize?.totalPrize, competition.prize?.token?.decimals)),
+            ticker: competition.prize?.token?.symbol,
+          })
+          break
+        case 1:
+          subText = t('Thenian Is Competing', {
+            totalPrize: formatAmount(fromWei(competition.prize?.totalPrize, competition.prize?.token?.decimals)),
+            ticker: competition.prize?.token?.symbol,
+            participantCount: competition.participantCount,
+          })
+          break
+        default:
+          subText = t('Thenian Are Competing', {
+            totalPrize: formatAmount(fromWei(competition.prize?.totalPrize, competition.prize?.token?.decimals)),
+            ticker: competition.prize?.token?.symbol,
+            participantCount: competition.participantCount,
+          })
+          break
+      }
+      return {
+        heading: t('Live'),
+        text: t('Competition Has Started'),
+        subText,
+      }
+    }
+    if (eventType === EVENT_TYPES.ENDED) {
+      let subText = null
+      let text = null
+
+      if (isJoined && !isClaimed) {
+        text = t('Claim Your Rewards')
+        subText = t('You Have Won')
+      } else {
+        switch (competition.participantCount) {
+          case 0:
+            subText = t('No Thenians Have Competed', {
+              totalPrize: formatAmount(fromWei(competition.prize?.totalPrize, competition.prize?.token?.decimals)),
+              ticker: competition.prize?.token?.symbol,
+            })
+            break
+          case 1:
+            subText = t('Thenian Has Competed', {
+              totalPrize: formatAmount(fromWei(competition.prize?.totalPrize, competition.prize?.token?.decimals)),
+              ticker: competition.prize?.token?.symbol,
+              participantCount: competition.participantCount,
+            })
+            break
+          default:
+            subText = t('Thenians Have Competed', {
+              totalPrize: formatAmount(fromWei(competition.prize?.totalPrize, competition.prize?.token?.decimals)),
+              ticker: competition.prize?.token?.symbol,
+              participantCount: competition.participantCount,
+            })
+            break
+        }
+      }
+
+      return {
+        heading: t('Ended'),
+        text,
+        subText,
+      }
+    }
+  }, [
+    competition.participantCount,
+    competition.prize?.token?.decimals,
+    competition.prize?.token?.symbol,
+    competition.prize?.totalPrize,
+    eventType,
+    isClaimed,
+    isInRegistration,
+    isJoined,
+    t,
+  ])
+
   const onShare = useCallback(() => {
     navigator.clipboard.writeText(window.location.href)
     successToast(t('Link Has Been Copied'))
   }, [t])
+
+  const buttonByStatus = useMemo(() => {
+    if (isHosting) {
+      return (
+        <PrimaryButton className='w-full' onClick={onShare}>
+          {t('Share')}
+        </PrimaryButton>
+      )
+    }
+
+    if (!isHosting && eventType === EVENT_TYPES.LIVE) {
+      return <PrimaryButton className='w-full'>{t('Trade Now')}</PrimaryButton>
+    }
+
+    if (!isHosting && eventType !== EVENT_TYPES.LIVE && isInRegistration) {
+      return (
+        <PrimaryButton className='w-full' disabled={isFull}>
+          {isFull ? t('This Competition Is Full') : t('Register')}
+        </PrimaryButton>
+      )
+    }
+
+    if (!isJoined && eventType === EVENT_TYPES.ENDED) {
+      return (
+        <SecondaryButton className='w-full' disabled>
+          {t('Trading Competition Has Ended')}
+        </SecondaryButton>
+      )
+    }
+    if (isJoined && eventType === EVENT_TYPES.ENDED) {
+      return <PrimaryButton className='bg-green w-full'>{t('Claim Rewards')}</PrimaryButton>
+    }
+  }, [eventType, isFull, isHosting, isInRegistration, isJoined, onShare, t])
 
   useEffect(() => {
     const progress = progressBarRef.current
@@ -59,37 +182,14 @@ function Sidebar({ competition }) {
 
   return (
     <div className='col-span-12 mt-2 lg:col-span-5'>
-      <h3 className='mb-5'>{t('Registration')}</h3>
+      <h3 className='mb-5'>{headingAndText.heading}</h3>
       <Box className='flex flex-col space-y-5'>
-        <Box className='flex flex-col space-y-2 border border-primary-800 bg-primary-950'>
-          {eventType === EVENT_TYPES.LIVE && !isJoined && (
-            <TextHeading className='text-xl'>{t('Competition Has Started')}</TextHeading>
-          )}
-          {eventType === EVENT_TYPES.ENDED && !isJoined && (
-            <>
-              <TextHeading className='text-xl'>{t('Ended')}</TextHeading>
-              <TextHeading className='text-base'>
-                {t('ParticipantCount Has Competed', {
-                  participantCount: competition.participantCount,
-                  totalPrize: formatAmount(fromWei(competition.prize?.totalPrize, competition.prize?.token?.decimals)),
-                  ticker: competition.prize?.token?.symbol,
-                })}
-              </TextHeading>
-            </>
-          )}
-          {isJoined && (
-            <>
-              <TextHeading className='text-xl'>
-                {eventType === EVENT_TYPES.ENDED ? t('Claim Your Rewards') : t('Competition Joined')}
-              </TextHeading>
-              <TextHeading className='text-base'>
-                {eventType === EVENT_TYPES.ENDED
-                  ? t('You Have Won')
-                  : t('You Joined This Competition On', { date: 'Nov 13, 2024' })}
-              </TextHeading>
-            </>
-          )}
-        </Box>
+        {(headingAndText.subText || headingAndText.text) && (
+          <Box className='flex flex-col space-y-2 border border-primary-800 bg-primary-950'>
+            {headingAndText.text && <TextHeading className='text-xl'>{headingAndText.text}</TextHeading>}
+            {headingAndText.subText && <TextHeading className='text-base'>{headingAndText.subText}</TextHeading>}
+          </Box>
+        )}
 
         <div className='flex flex-col items-center justify-center gap-2'>
           <div className='h-3 w-full rounded-md bg-neutral-500'>
@@ -109,27 +209,7 @@ function Sidebar({ competition }) {
             }
           />
         )}
-        {isHosting && (
-          <PrimaryButton className='w-full' onClick={onShare}>
-            {t('Share')}
-          </PrimaryButton>
-        )}
-        {!isHosting && eventType === EVENT_TYPES.LIVE && (
-          <PrimaryButton className='w-full'>{t('Trade Now')}</PrimaryButton>
-        )}
-        {!isHosting && isInRegistration && eventType !== EVENT_TYPES.LIVE && (
-          <PrimaryButton className='w-full' disabled={isFull}>
-            {isFull ? t('This Competition Is Full') : t('Register')}
-          </PrimaryButton>
-        )}
-        {!isJoined && eventType === EVENT_TYPES.ENDED && (
-          <SecondaryButton className='w-full' disabled>
-            {t('Trading Competition Has Ended')}
-          </SecondaryButton>
-        )}
-        {isJoined && eventType === EVENT_TYPES.ENDED && (
-          <PrimaryButton className='bg-green w-full'>{t('Claim Rewards')}</PrimaryButton>
-        )}
+        {buttonByStatus}
       </Box>
     </div>
   )
