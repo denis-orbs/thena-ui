53a83a33995a2249bd694eb5ed90b0ee191ae516
namnh <nam.nh1@zinza.com.vn>
2024-04-08 18:58:53 +0700

    fix: hide hours and minute if rezo

--- src/modules/CountDown/index.jsx
+++ src/modules/CountDown/index.jsx
@@ -33,19 +33,22 @@ export function Countdown({ timestamp }) {
         </Box>
       )}
 
-      <Box className='flex h-24 flex-col items-center items-center justify-center bg-neutral-800'>
-        <Paragraph className='inline-block bg-gradient-to-r from-[#C72AD0] to-[#AA23DB] bg-clip-text bg-clip-text text-3xl font-bold text-transparent'>
-          {hours}
-        </Paragraph>
-        <Paragraph className='text-base'>{hours <= 1 ? t('Hour') : t('Hours')}</Paragraph>
-      </Box>
-
-      <Box className='flex h-24 flex-col items-center items-center justify-center bg-neutral-800'>
-        <Paragraph className='inline-block bg-gradient-to-r from-[#C72AD0] to-[#AA23DB] bg-clip-text bg-clip-text text-3xl font-bold text-transparent'>
-          {minutes}
-        </Paragraph>
-        <Paragraph className='text-base'>{minutes <= 1 ? t('Minute') : t('Minutes')}</Paragraph>
-      </Box>
+      {days === 0 && hours === 0 ? null : (
+        <Box className='flex h-24 flex-col items-center items-center justify-center bg-neutral-800'>
+          <Paragraph className='inline-block bg-gradient-to-r from-[#C72AD0] to-[#AA23DB] bg-clip-text bg-clip-text text-3xl font-bold text-transparent'>
+            {hours}
+          </Paragraph>
+          <Paragraph className='text-base'>{hours <= 1 ? t('Hour') : t('Hours')}</Paragraph>
+        </Box>
+      )}
+      {days === 0 && hours === 0 && minutes === 0 ? null : (
+        <Box className='flex h-24 flex-col items-center items-center justify-center bg-neutral-800'>
+          <Paragraph className='inline-block bg-gradient-to-r from-[#C72AD0] to-[#AA23DB] bg-clip-text bg-clip-text text-3xl font-bold text-transparent'>
+            {minutes}
+          </Paragraph>
+          <Paragraph className='text-base'>{minutes <= 1 ? t('Minute') : t('Minutes')}</Paragraph>
+        </Box>
+      )}
 
       <Box className='flex h-24 flex-col items-center items-center justify-center bg-neutral-800'>
         <Paragraph className='inline-block bg-gradient-to-r from-[#C72AD0] to-[#AA23DB] bg-clip-text bg-clip-text text-3xl font-bold text-transparent'>
