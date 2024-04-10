8cef66da7fc19c782632b40be968273c8507b597
namnh <nam.nh1@zinza.com.vn>
2024-04-10 16:55:18 +0700

    fix: error import

--- src/app/arena/trading-competitions/[id]/leaderboard/page.jsx
+++ src/app/arena/trading-competitions/[id]/leaderboard/page.jsx
@@ -3,7 +3,7 @@
 import { useParams } from 'next/navigation'
 import React from 'react'
 
-import { useTradingCompetitionLeaderBoard } from '@/hooks/useTradingCompetitionLeaderboard'
+import { useTradingCompetitionLeaderBoard } from '@/hooks/trade/useTradingCompetitionLeaderboard'
 import { LeaderBoard } from '@/modules/TradingCompetition/LeaderBoard'
 
 function LeaderBoardPage() {
