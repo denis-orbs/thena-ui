d38d5fe4071d09c4e83d1a242c3aca269427d19e
namnh <nam.nh1@zinza.com.vn>
2024-04-29 09:42:04 +0700

    fix: admin edit page 404

---
+++ src/app/arena/admin/edit/page.jsx
@@ -0,0 +1,80 @@
+'use client'
+
+import { gql } from 'graphql-request'
+import React from 'react'
+import useSWR from 'swr'
+
+import Loading from '@/app/loading'
+import { v4Client } from '@/lib/graphql'
+import { getFromSessionStorage } from '@/lib/helper'
+import useWallet from '@/lib/wallets/useWallet'
+import { EditProfile } from '@/modules/Profile/EditProfile'
+
+const V4_USER_RANK = gql`
+  query V4_UER_RANK($id: String!) {
+    tradeRankByAddress(address: $id) {
+      rank
+    }
+  }
+`
+
+const V4_USER_INFO = gql`
+  query V4_USER($id: String!) {
+    userById(id: $id) {
+      id
+      firstInteractAt
+      biography
+      timezone
+      websiteUrl
+      xProfileUrl
+      username
+      theme
+      nameColor
+      avatar
+      thenianNfts {
+        meatadata {
+          image
+        }
+        id
+      }
+      usernameNfts {
+        name
+        id
+      }
+    }
+  }
+`
+
+const fetchUserInfo = async id => {
+  try {
+    const { userById } = await v4Client.request(
+      V4_USER_INFO,
+      { id: id.toLowerCase() },
+      {
+        authorization: getFromSessionStorage('token') ? `Bearer ${getFromSessionStorage('token')}` : '',
+      },
+    )
+
+    const { tradeRankByAddress } = await v4Client.request(V4_USER_RANK, { id: id.toLowerCase() })
+
+    return { ...userById, rank: tradeRankByAddress?.[0]?.rank ?? '-' }
+  } catch (error) {
+    return { error: true }
+  }
+}
+
+function EditAdminProfile() {
+  const { account } = useWallet()
+
+  const { data: userInfo, isLoading } = useSWR(['edit user info'], () => fetchUserInfo(account?.toLowerCase()), {
+    refreshInterval: 60000,
+  })
+
+  if (!userInfo || isLoading) {
+    return <Loading />
+  }
+
+  return <EditProfile userInfo={userInfo} isAdmin />
+}
+
+export default EditAdminProfile
