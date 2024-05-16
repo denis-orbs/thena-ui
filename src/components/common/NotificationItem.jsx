613624a31bb841c1230d446430c469386fbf03fc
namnh <nam.nh1@zinza.com.vn>
2024-05-16 14:28:44 +0700

    Add user avatar default to notification item

--- src/components/common/NotificationItem.jsx
+++ src/components/common/NotificationItem.jsx
@@ -2,13 +2,13 @@
 
 import moment from 'moment'
 import Link from 'next/link'
+import Avatar from 'public/images/home/stats/socials/social-1.png'
 import 'moment/locale/zh-cn'
 
 import { cn } from '@/lib/utils'
 import { useLocaleSettings } from '@/state/settings/hooks'
-import { UserIcon } from '@/svgs'
 
-import Highlight from '../highlight'
+import CircleImage from '../image/CircleImage'
 import { Paragraph, TextHeading } from '../typography'
 
 function NotificationItem({ notification, markRead }) {
@@ -24,9 +24,9 @@ function NotificationItem({ notification, markRead }) {
             "relative after:absolute after:right-5 after:top-1/2 after:h-2 after:w-2 after:rounded-full after:bg-primary-600 after:content-['']",
         )}
       >
-        <Highlight className='bg-gradient-to-t from-[#9A5EFF] to-primary-600'>
-          <UserIcon className='h-4 w-4 text-black' />
-        </Highlight>
+        <div className='h-12 w-12'>
+          <CircleImage src={Avatar} alt='avatar' className='size-9 h-9 w-9' />
+        </div>
         <div className='flex flex-col gap-1 p-2'>
           <TextHeading className='text-wrap break-all'>{notification.content}</TextHeading>
           <Paragraph className='text-sm'>{moment(notification.timestamp).locale(locale).fromNow()}</Paragraph>
