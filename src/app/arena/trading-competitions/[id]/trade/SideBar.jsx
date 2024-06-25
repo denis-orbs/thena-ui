30b28d2cbfb9e0d8fa25f8bb3e7f59d87ac05072
HieuPM <hieu.pm@zinza.com.vn>
2024-06-25 17:55:56 +0700

    fix toWei with decimals in arena page

--- src/app/arena/trading-competitions/[id]/trade/SideBar.jsx
+++ src/app/arena/trading-competitions/[id]/trade/SideBar.jsx
@@ -137,7 +137,7 @@ export function SideBar({
   const { data: oneInchQuoteData, isLoading: isLoadingOneInch } = useGet1InchSwapData(
     fromAddress,
     toAsset?.address,
-    toWei(debouncedFromTokenAmount),
+    toWei(debouncedFromTokenAmount, fromAsset?.decimals),
     slippage,
     networkId,
     tcAddress,
@@ -259,7 +259,7 @@ export function SideBar({
         isSuccess = await onSwapAlgebra(
           fromAsset,
           toAsset,
-          toWei(debouncedFromTokenAmount),
+          toWei(debouncedFromTokenAmount, fromAsset?.decimals),
           toWei(minimumReceived),
           tcAddress,
           deadline,
