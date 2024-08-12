aa031f4246e0e40cc882035a5594a368cc3f6a24
HieuPM <hieu.pm@zinza.com.vn>
2024-08-12 16:13:44 +0700

    feat: layout THE home story

--- src/app/story/page.jsx
+++ src/app/story/page.jsx
@@ -9,11 +9,15 @@ function StoryPage() {
     <div className='mt-6 flex flex-col gap-4'>
       <div className='w-full'>
         <div className='mt-6 grid grid-cols-12 gap-8 lg:gap-12'>
-          <div className='col-span-12 my-auto px-10 lg:col-span-7'>
-            <p className='text-purple-600'>The story of Thena</p>
-            <p className='text-4xl font-black text-gray-900 dark:text-white'>Join the Adventure and Earn Rewards!</p>
-            <p>Brief Campaign Description Brief Campaign DescriptionBrief Campaign Description</p>
-            <p>DescriptionBrief Campaign Description</p>
+          <div className='col-span-12 my-auto px-20 lg:col-span-7'>
+            <small className='uppercase text-purple-600'>The story of Thena</small>
+            <p className='mb-3 text-4xl font-black text-gray-900 dark:text-white'>
+              Join the Adventure and Earn Rewards!
+            </p>
+            <small className='text-gray-300'>
+              Brief Campaign Description Brief Campaign DescriptionBrief Campaign Description
+            </small>
+            <small className='text-gray-300'>DescriptionBrief Campaign Description</small>
           </div>
           <StoryRegister />
         </div>
--- src/modules/Story/StoryRegister.jsx
+++ src/modules/Story/StoryRegister.jsx
@@ -1,55 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Box from '@/components/box'
 import { SecondaryButton } from '@/components/buttons/Button'
+import Input from '@/components/input'
+
+const initialFormState = {
+  evmAddress: '',
+  country: null,
+  email: '',
+}
 
 export default function StoryRegister() {
+  const [formState, setFormState] = useState(initialFormState)
+
+  const handleChange = name => event => {
+    setFormState(prev => ({
+      ...prev,
+      [name]: event.target.value,
+    }))
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    console.log(formState)
+  }
   return (
-    <Box className='z-10 col-span-12 lg:sticky lg:top-56 lg:col-span-5 lg:max-h-[500px]'>
+    <Box className='z-10 col-span-12 my-auto lg:sticky lg:top-56 lg:col-span-5 lg:max-h-[500px]'>
       <h2 className='mb-6'>Register Now</h2>
-      <div className='mb-6'>
-        <label htmlFor='evmAddress' className='mb-1 block text-sm font-medium text-gray-900 dark:text-white'>
-          Your EVM Address
-        </label>
-        <input
-          type='text'
-          id='evmAddress'
-          className='w-full cursor-text rounded-lg border border-neutral-700 bg-neutral-700 py-3 text-neutral-50 placeholder-neutral-400 caret-transparent focus:border-neutral-500'
-          placeholder='EVM Address'
-          required
-        />
-      </div>
-      <div className='mb-6'>
-        <div>
-          <label htmlFor='countries' className='mb-1 block text-sm font-medium text-gray-900 dark:text-white'>
-            Select Country
+      <form onSubmit={handleSubmit}>
+        <div className='mb-6'>
+          <label htmlFor='evmAddress' className='mb-1 block text-sm font-medium text-gray-900 dark:text-white'>
+            Your EVM Address
           </label>
-          <select
-            id='countries'
-            className='w-full cursor-pointer rounded-lg border border-neutral-700 bg-neutral-700 py-3 text-neutral-50 placeholder-neutral-400 caret-transparent focus:border-neutral-500'
-          >
-            <option selected>Choose</option>
-            <option value='US'>United States</option>
-            <option value='CA'>Canada</option>
-            <option value='FR'>France</option>
-            <option value='DE'>Germany</option>
-          </select>
+          <Input
+            type='text'
+            id='evmAddress'
+            placeholder='EVM Address'
+            val={formState.evmAddress}
+            onChange={handleChange('evmAddress')}
+            required
+          />
         </div>
-      </div>
-      <div className='mb-6'>
-        <label htmlFor='emailAddress' className='mb-1 block text-sm font-medium text-gray-900 dark:text-white'>
-          Email Address
-        </label>
-        <input
-          name='email'
-          type='email'
-          id='emailAddress'
-          className='w-full cursor-text rounded-lg border border-neutral-700 bg-neutral-700 py-3 text-neutral-50 placeholder-neutral-400 caret-transparent focus:border-neutral-500'
-          placeholder='Email Address'
-          required
-        />
-      </div>
-      <SecondaryButton className='w-full'>Join now</SecondaryButton>
+        <div className='mb-6'>
+          <div>
+            <label htmlFor='countries' className='mb-1 block text-sm font-medium text-gray-900 dark:text-white'>
+              Select Country
+            </label>
+            <select
+              id='countries'
+              name='country'
+              className='w-full cursor-pointer rounded-lg border border-neutral-700 bg-neutral-700 py-3 text-neutral-50 placeholder-neutral-400 caret-transparent focus:border-neutral-500'
+              value={formState.country}
+              onChange={handleChange('country')}
+            >
+              <option selected>Choose</option>
+              <option value='US'>United States</option>
+              <option value='CA'>Canada</option>
+              <option value='FR'>France</option>
+              <option value='DE'>Germany</option>
+            </select>
+          </div>
+        </div>
+        <div className='mb-6'>
+          <label htmlFor='emailAddress' className='mb-1 block text-sm font-medium text-gray-900 dark:text-white'>
+            Email Address
+          </label>
+          <Input
+            type='email'
+            id='emailAddress'
+            name='email'
+            placeholder='EVM Address'
+            val={formState.email}
+            onChange={handleChange('email')}
+            required
+          />
+        </div>
+        <SecondaryButton type='submit' className='w-full'>
+          Join now
+        </SecondaryButton>
+      </form>
     </Box>
   )
 }
