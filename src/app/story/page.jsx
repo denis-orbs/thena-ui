c4e0a2971163c16e99f2f355ff172276a2030d97
HieuPM <hieu.pm@zinza.com.vn>
2024-08-13 16:00:12 +0700

    fix: translation, color

--- src/app/story/page.jsx
+++ src/app/story/page.jsx
@@ -13,9 +13,7 @@ function StoryPage() {
         <div className='mt-6 grid grid-cols-12 gap-8 lg:gap-12'>
           <div className='col-span-12 my-auto px-20 lg:col-span-7'>
             <small className='font-semibold uppercase text-gradient'>{t('The story of Thena')}</small>
-            <p className='mb-3 text-4xl font-black text-gray-900 dark:text-white'>
-              {t('Join the Adventure and Earn Rewards!')}
-            </p>
+            <p className='mb-3 text-4xl font-black text-white'>{t('Join the Adventure and Earn Rewards!')}</p>
             <small>{t('Brief Campaign Description Brief Campaign DescriptionBrief Campaign Description')}</small>
             <small>{t('DescriptionBrief Campaign Description')}</small>
           </div>
