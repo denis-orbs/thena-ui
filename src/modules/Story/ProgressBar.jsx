bd21758b8916158367711abdbbb02d195669dd87
HieuPM <hieu.pm@zinza.com.vn>
2024-08-19 10:38:28 +0700

    fix: remove unused files

--- src/modules/Story/ProgressBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ProgressBar({ process, bgProcess = 'bg-[#35243D]', bgComplete = 'bg-custom-gradient' }) {
-  return (
-    <div className={`h-2 w-full rounded-full ${bgProcess}`}>
-      <div className={`h-2 rounded-full ${bgComplete}`} style={{ width: process }} />
-    </div>
-  )
-}
