4f36acaae5116bf42e2cd666a385f6ec9a4e74af
HieuPM <hieu.pm@zinza.com.vn>
2024-08-26 18:04:48 +0700

    fix: update logic submenu

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -655,7 +655,7 @@ function Header() {
           <Tabs data={submenus} size={SizeTypes.Medium} />
         </div>
       )}
-      {pathname.startsWith('/story') && isUpcoming && isRegistered && (
+      {pathname.startsWith('/story') && !isUpcoming && isRegistered && (
         <div className='fixed top-[64px] z-[45] w-full bg-neutral-900 p-4 backdrop-blur-2xl lg:top-[92px] lg:flex lg:px-60 lg:py-5'>
           <Tabs data={storySubmenus} size={SizeTypes.Medium} />
         </div>
