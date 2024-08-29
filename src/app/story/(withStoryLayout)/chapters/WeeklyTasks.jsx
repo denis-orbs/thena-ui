afc645313b34d0b9b17c2ecace5a847c1dcb6f77
HieuPM <hieu.pm@zinza.com.vn>
2024-08-29 19:00:06 +0700

    fix: hardcode chapters

--- src/app/story/(withStoryLayout)/chapters/WeeklyTasks.jsx
+++ src/app/story/(withStoryLayout)/chapters/WeeklyTasks.jsx
@@ -59,6 +59,130 @@ export function WeeklyTasks({ chapters, selectedChapterIndex, setSelectedChapter
     [chapters],
   )
 
+  const hardChapters = [
+    {
+      id: '1',
+      index: 1,
+      name: 'Enter ARENA',
+      startTimestamp: '2024-09-02T12:00:00.000000Z',
+      endTimestamp: '2024-09-09T12:00:00.000000Z',
+      tasks: [
+        {
+          actionHandle: 'arena/thena-id/mint',
+          chapter: 1,
+          id: '0000000',
+          index: 0,
+          name: 'Mint thena ID',
+          rewardType: ['Point', 'Fragment'],
+          rewardAmount: [0, 1],
+          type: 'Main',
+          isCompleted: false,
+        },
+        {
+          actionHandle: 'arena/trading-competitions/0xf53a5053c09d9215b58da77a48aba3e66f355864-1',
+          chapter: 1,
+          id: '0000001',
+          index: 1,
+          name: 'Register for a BSC TC',
+          rewardType: ['Point', 'Fragment'],
+          rewardAmount: [10, 0],
+          type: 'Main',
+          isCompleted: false,
+        },
+        {
+          actionHandle: 'tweet',
+          chapter: 1,
+          id: '0000002',
+          index: 2,
+          name: 'Tweet with hashtag StoryofTHENA',
+          rewardType: ['Point', 'Fragment'],
+          rewardAmount: [10, 0],
+          type: 'Main',
+          isCompleted: false,
+        },
+        {
+          actionHandle: 'swap',
+          chapter: null,
+          id: '2000001',
+          index: 0,
+          name: 'Daily Swap-in',
+          rewardType: ['Point', 'Fragment'],
+          rewardAmount: [10, 0],
+          type: 'Side',
+          isCompleted: false,
+        },
+        {
+          actionHandle: 'story/referral',
+          chapter: null,
+          id: '2000002',
+          index: 1,
+          name: 'Refer your friends',
+          rewardType: ['Point', 'Fragment'],
+          rewardAmount: [10, 0],
+          type: 'Side',
+          isCompleted: false,
+        },
+      ],
+      isCompleted: false,
+      available: false,
+    },
+    {
+      id: '2',
+      index: 2,
+      name: 'Concentrating on Liquidity',
+      startTimestamp: '2024-09-02T12:00:00.000000Z',
+      endTimestamp: '2024-09-09T12:00:00.000000Z',
+      tasks: [
+        {
+          actionHandle: 'swap',
+          chapter: 2,
+          id: '0000003',
+          index: 0,
+          name: 'Swap THE',
+          rewardType: ['Point', 'Fragment'],
+          rewardAmount: [10, 0],
+          type: 'Main',
+          isCompleted: false,
+        },
+        {
+          actionHandle: 'pools/0x51bd5e6d3da9064d59bcaa5a76776560ab42ceb8',
+          chapter: 2,
+          id: '0000004',
+          index: 1,
+          name: 'Stake into THE/BNB (ICHI)',
+          rewardType: ['Point', 'Fragment'],
+          rewardAmount: [10, 0],
+          type: 'Main',
+          isCompleted: false,
+        },
+        {
+          actionHandle: 'swap',
+          chapter: null,
+          id: '2000001',
+          index: 0,
+          name: 'Daily Swap-in',
+          rewardType: ['Point', 'Fragment'],
+          rewardAmount: [10, 0],
+          type: 'Side',
+          isCompleted: false,
+        },
+        {
+          actionHandle: 'story/referral',
+          chapter: null,
+          id: '2000002',
+          index: 1,
+          name: 'Refer your friends',
+          rewardType: ['Point', 'Fragment'],
+          rewardAmount: [10, 0],
+          type: 'Side',
+          isCompleted: false,
+        },
+      ],
+      isCompleted: false,
+      available: false,
+    },
+  ]
+
   return (
     <div className='mt-[63px]'>
       <TextHeading className='block font-archia text-3xl font-semibold leading-9'>{t('Weekly Tasks')}</TextHeading>
@@ -68,7 +192,7 @@ export function WeeklyTasks({ chapters, selectedChapterIndex, setSelectedChapter
       <div className='mt-5 grid grid-cols-12 gap-8 lg:gap-12'>
         <div className='col-span-12 lg:col-span-7'>
           <ChapterTabNavigator
-            chapters={chapters}
+            chapters={hardChapters}
             selectedChapterIndex={selectedChapterIndex}
             setSelectedChapterIndex={setSelectedChapterIndex}
           />
