085ff6a1a9a6b8c35e56dc34a6b0268880ef01c4
HieuPM <hieu.pm@zinza.com.vn>
2024-09-30 10:26:22 +0700

    feat: `reward` chapter 3

--- src/app/story/(withStoryLayout)/rewards/chapters/RewardChapterDetail.jsx
+++ src/app/story/(withStoryLayout)/rewards/chapters/RewardChapterDetail.jsx
@@ -12,24 +12,24 @@ function RewardChapterDetail({ rewards, rewardsTimestamp }) {
       <div className='mb-4 mt-4 lg:mb-[60px] lg:mt-11'>
         <TextHeading className='font-archia text-3xl font-semibold'>
           <span>{t('Rewards in USD')}: </span>
-          <span className='text-primary-600'>{rewardsTimestamp ? '$16,000' : 'TBA'}</span>
+          <span className='text-primary-600'>${rewardsTimestamp ? rewards?.rewardsInUSD : 'TBA'}</span>
         </TextHeading>
       </div>
 
       <div className='grid grid-cols-2 lg:grid-cols-3'>
-        {rewards.map((reward, index) => (
+        {rewards?.items?.map((reward, index) => (
           <div
-            key={reward.id}
+            key={reward?.id}
             className={cn(
               'flex items-start justify-center',
               index === rewards.length - 1 ? 'col-span-2 lg:col-span-1' : 'col-span-1',
             )}
           >
             <HowItWorksItem
-              key={reward.id}
-              icon={reward.icon}
-              title={reward.name}
-              description={reward.description}
+              key={reward?.id}
+              icon={reward?.icon}
+              title={reward?.name}
+              description={reward?.description}
               className='w-auto p-0 md:w-full lg:p-6'
             />
           </div>
