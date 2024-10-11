3fa196edd13a0bfad7a55e8ee88018662ae1303b
HieuPM <hieu.pm@zinza.com.vn>
2024-10-11 17:58:16 +0700

    fix: disable rewards for some pools

--- src/app/dashboard/vote/page.jsx
+++ src/app/dashboard/vote/page.jsx
@@ -266,10 +266,22 @@ export default function VotePage() {
         ),
         rewards: (
           <div className='flex items-center gap-1'>
-            <Paragraph>${formatAmount(pool.title === 'DefiEdge' ? 0 : pool.gauge.bribeUsd)}</Paragraph>
-            {pool.gauge.bribeUsd.gt(0) && pool.title !== 'DefiEdge' && (
-              <InfoIcon className='h-4 w-4 stroke-neutral-400' data-tooltip-id={`projected-${pool.gauge.address}`} />
-            )}
+            <Paragraph>
+              $
+              {formatAmount(
+                pool.title === 'DefiEdge' ||
+                  pool.address === '0xf7369b1d005f2cbb1887233b5aa0cb0b39fb9891' ||
+                  pool.address === '0xac7042fed4e724107fd5778f4a9cad894a5e18ab'
+                  ? 0
+                  : pool.gauge.bribeUsd,
+              )}
+            </Paragraph>
+            {pool.gauge.bribeUsd.gt(0) &&
+              pool.title !== 'DefiEdge' &&
+              pool.address !== '0xf7369b1d005f2cbb1887233b5aa0cb0b39fb9891' &&
+              pool.address !== '0xac7042fed4e724107fd5778f4a9cad894a5e18ab' && (
+                <InfoIcon className='h-4 w-4 stroke-neutral-400' data-tooltip-id={`projected-${pool.gauge.address}`} />
+              )}
             <CustomTooltip className='min-w-[136px]' id={`projected-${pool.gauge.address}`}>
               <div className='flex flex-col gap-1'>
                 {pool.gauge.bribes && pool.gauge.bribes.bribe && (
@@ -302,7 +314,16 @@ export default function VotePage() {
         ),
         estimate: (
           <div className='flex flex-col'>
-            <Paragraph>${formatAmount(pool.title === 'DefiEdge' ? 0 : pool.votes.perRewards)}</Paragraph>
+            <Paragraph>
+              $
+              {formatAmount(
+                pool.title === 'DefiEdge' ||
+                  pool.address === '0xf7369b1d005f2cbb1887233b5aa0cb0b39fb9891' ||
+                  pool.address === '0xac7042fed4e724107fd5778f4a9cad894a5e18ab'
+                  ? 0
+                  : pool.votes.perRewards,
+              )}
+            </Paragraph>
             <TextSubHeading className='text-base leading-tight'>{t('Per 1000 Votes')}</TextSubHeading>
           </div>
         ),
