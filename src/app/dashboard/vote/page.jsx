0004e92546e67b55fae95cd725a03e54d56d955c
HieuPM <hieu.pm@zinza.com.vn>
2024-10-31 17:23:08 +0700

    Revert "Merge pull request #93 from ThenafiBNB/feat/update-rewards-xvs"
    This reverts commit c954b2d7a52d3b323b30a87f7ddae09886e01e61, reversing
    changes made to 30fd89a62c6642637f7eb76981b9b7c20560b9d2.

--- src/app/dashboard/vote/page.jsx
+++ src/app/dashboard/vote/page.jsx
@@ -143,30 +143,6 @@ export default function VotePage() {
         .filter(pair => pair.title !== 'DefiEdge') // hide all DeFi Edge
         .filter(pair => pair.gauge.address !== zeroAddress && pair.gauge.isAlive)
         .map(pair => {
-          // TODO: Hard-coded for BNB/XVS Narrow
-          if (pair.address === '0xbdbeb09a06dbd397157cedc306920c20d05aee7d') {
-            pair.gauge.bribeUsd = new BigNumber('2367.7511746927')
-            pair.gauge.voteApr = pair.gauge.bribeUsd.times(52).div(pair.gauge.weight.times(0.245)).times(100)
-            if (pair.gauge.bribes) {
-              pair.gauge.bribes = {
-                ...pair.gauge.bribes,
-                fee: [
-                  {
-                    address: '0xcF6BB5389c92Bdda8a3747Ddb454cB7a64626C63',
-                    decimals: 18,
-                    amount: 169.79444915,
-                    symbol: 'XVS',
-                  },
-                  {
-                    address: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
-                    decimals: 18,
-                    amount: 1.92064285,
-                    symbol: 'WBNB',
-                  },
-                ],
-              }
-            }
-          }
           const perRewards = pair.gauge.bribeUsd.div(pair.gauge.weight.plus(1000)).times(1000)
           let votes = {
             weight: new BigNumber(0),
