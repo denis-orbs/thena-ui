fef181168a286d2e0986c96449353b282ddb0f8e
HieuPM <hieu.pm@zinza.com.vn>
2024-12-19 18:19:56 +0700

    fix: show whitelisted token first

--- src/lib/api.js
+++ src/lib/api.js
@@ -22,7 +22,7 @@ export const fetchAssets = async (networkId, liquidityHubEnabled) => {
       liquidityHub.getTokens(liquidityHubEnabled),
     ])
     const assets = _.filter(
-      _.uniqBy([...liquidityHubTokens, ...assetsCall.data], it => it.address.toLowerCase()),
+      _.uniqBy([...assetsCall.data, ...liquidityHubTokens], it => it.address.toLowerCase()),
       it => it.chainId === networkId,
     )
     // both lists have CAKE, so we need to merge them
