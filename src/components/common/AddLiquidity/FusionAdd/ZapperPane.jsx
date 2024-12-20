6aded2a6f04a32d053d6a77acff0cae55b54eb10
namnh <nam.nh1@zinza.com.vn>
2024-12-20 17:32:30 +0700

    feat: add liquidity by zapper

---
+++ src/components/common/AddLiquidity/FusionAdd/ZapperPane.jsx
@@ -0,0 +1,136 @@
+import { useTranslations } from 'next-intl'
+import React, { useMemo, useState } from 'react'
+
+import { PrimaryButton } from '@/components/buttons/Button'
+import ConnectButton from '@/components/buttons/ConnectButton'
+import { ThreeIconGroup } from '@/components/icongroup/ThreeIconGroup'
+import TokenInput from '@/components/input/TokenInput'
+import Tabs from '@/components/tabs'
+import { Paragraph, TextHeading, TextSubHeading } from '@/components/typography'
+import useWallet from '@/hooks/useWallet'
+import { useGetZapInRoute, useZapperAddLiquidity } from '@/hooks/zapper/useZapper'
+import { cn, fromWei } from '@/lib/utils'
+import { ArrowRightIcon } from '@/svgs'
+
+function ZapperPane({ asset1, asset2, slippage, poolId, tickLower, tickUpper, deadline, mintInfo }) {
+  const t = useTranslations()
+  const { account } = useWallet()
+  const [tokensData] = useState([asset1, asset2])
+  const [tokenDeposit, setTokenDeposit] = useState(asset1)
+  const { handleAddLiquidity } = useZapperAddLiquidity()
+
+  const [amount, setAmount] = useState(0)
+
+  const { data, isFetching } = useGetZapInRoute({
+    tickLower,
+    tickUpper,
+    poolId,
+    tokenIn: tokenDeposit,
+    amountIn: amount,
+    slippage: slippage * 100,
+  })
+
+  const percents = useMemo(
+    () => [
+      {
+        label: '10%',
+        onClickHandler: () => setAmount(tokenDeposit?.balance.times(0.1).toString(10)),
+      },
+      {
+        label: '25%',
+        onClickHandler: () => setAmount(tokenDeposit?.balance.times(0.25).toString(10)),
+      },
+      {
+        label: '50%',
+        onClickHandler: () => setAmount(tokenDeposit?.balance.times(0.5).toString(10)),
+      },
+      {
+        label: 'Max',
+        onClickHandler: () => setAmount(tokenDeposit?.balance.toString(10)),
+      },
+    ],
+    [tokenDeposit?.balance, setAmount],
+  )
+
+  return (
+    <div className='flex flex-col gap-2'>
+      <div className='flex flex-row justify-between'>
+        <TextHeading>{t('Deposit Token')}</TextHeading> <Tabs data={percents} />
+      </div>
+      <div className='relative flex w-full flex-col gap-2'>
+        <TokenInput
+          asset={tokenDeposit}
+          setAsset={setTokenDeposit}
+          amount={amount}
+          setAmount={setAmount}
+          autoFocus
+          assetData={tokensData}
+          assetNull
+        />
+      </div>
+
+      <ArrowRightIcon className='mx-auto h-5 w-5 rotate-90' />
+
+      <TextHeading>Liquidity</TextHeading>
+      <div className='flex flex-col gap-3 self-stretch rounded-xl border border-neutral-700 p-4'>
+        <div className='flex items-center justify-between gap-2'>
+          <input
+            type='number'
+            className='w-full flex-[4] border-0 bg-transparent p-0 text-xl text-neutral-50 placeholder-neutral-400'
+            placeholder='0.0'
+            value={fromWei(data?.positionDetails?.addedLiquidity ?? 0).toFixed(6)}
+            readOnly
+          />
+          <div
+            className={cn(
+              'inline-flex items-center justify-center gap-2',
+              'rounded-full bg-neutral-600 text-sm text-neutral-200',
+              'max-w-[60%] py-1.5 pl-1.5 pr-2',
+            )}
+          >
+            <ThreeIconGroup
+              logo1={asset1.logoURI}
+              logo2={asset2.logoURI}
+              classNames={{ image: 'w-6 h-6' }}
+              className='-space-x-1'
+            />
+            <span className='text-wrap'>
+              {asset1.symbol}-{asset2.symbol}
+            </span>
+          </div>
+        </div>
+
+        <div className='flex items-center justify-between gap-2'>
+          <TextSubHeading>${data?.positionDetails?.addedAmountUsd ?? 0}</TextSubHeading>
+        </div>
+      </div>
+
+      <div className='my-2 flex flex-row justify-between'>
+        <TextSubHeading>{t('Total Deposit')}</TextSubHeading>
+        <Paragraph>${data?.zapDetails?.finalAmountUsd ?? 0}</Paragraph>
+      </div>
+
+      {account ? (
+        <PrimaryButton
+          disabled={isFetching || !data?.route}
+          onClick={() => {
+            handleAddLiquidity({
+              route: data?.route,
+              mintInfo,
+              deadline,
+              amount,
+              token: tokenDeposit,
+            })
+          }}
+          className='w-full'
+        >
+          {t('Add Liquidity')}
+        </PrimaryButton>
+      ) : (
+        <ConnectButton className='w-full' />
+      )}
+    </div>
+  )
+}
+
+export default ZapperPane
---
+++ src/hooks/zapper/useZapper.jsx
@@ -0,0 +1,220 @@
+import { useMutation, useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import BigNumber from 'bignumber.js'
+import { useTranslations } from 'next-intl'
+import { useCallback, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { getAddress, maxUint256 } from 'viem'
+
+import { TXN_STATUS } from '@/constant'
+import { readCall, waitCall } from '@/lib/contractActions'
+import {
+  getERC20Contract,
+  getFarmingCenterContract,
+  getInsentiveContract,
+  getPositionManagerContract,
+} from '@/lib/contracts'
+import { NonfungiblePositionManager } from '@/lib/fusion/entities/nonfungiblePositionManager'
+import { fromWei, toWei } from '@/lib/utils'
+import { useTxn } from '@/state/transactions/hooks'
+
+import useWallet from '../useWallet'
+
+const BASE_ZAPPER_URL = 'https://zap-api.kyberswap.com/bsc/api/v1'
+
+// https://zap-api.kyberswap.com/bsc/api/v1/in/route?
+// dex=DEX_THENAALGEBRAINTEGRAL&
+// pool.id=0x9ea0f51fd2133d995cf00229bc523737415ad318&
+// position.tickLower=-64200&
+// position.tickUpper=64800&
+// tokensIn=0x55d398326f99059ff775485246999027b3197955&
+// amountsIn=10000000000000000000000&
+// slippage=100
+
+export const useGetZapInRoute = ({ tickLower, tickUpper, poolId, tokenIn, amountIn, slippage = 100 }) =>
+  useQuery({
+    queryKey: ['zapInRoute', tickLower, tickUpper, poolId, tokenIn, amountIn, slippage],
+    queryFn: async () => {
+      const amount = toWei(
+        new BigNumber(amountIn).decimalPlaces(tokenIn.decimals, BigNumber.ROUND_DOWN).toString(),
+        tokenIn.decimals,
+      )
+
+      // TODO: replace with pool address
+      const params = {
+        dex: 'DEX_THENAALGEBRAINTEGRAL',
+        'pool.id': '0x9ea0f51fd2133d995cf00229bc523737415ad318',
+        'position.tickLower': tickLower,
+        'position.tickUpper': tickUpper,
+        tokenIn: tokenIn.address,
+        amountIn: amount,
+        slippage,
+      }
+
+      const response = await axios.get(`${BASE_ZAPPER_URL}/in/route`, {
+        params,
+      })
+      return response.data?.data
+    },
+    enabled: Boolean(!!poolId && !!tickLower && !!tickUpper && !!tokenIn && !!amountIn),
+  })
+
+export const useBuildZapInRoute = () =>
+  useMutation({
+    mutationFn: async ({ route, sender, recipient, deadline }) => {
+      const response = await axios.post(`${BASE_ZAPPER_URL}/in/route/build`, {
+        body: {
+          sender,
+          recipient,
+          route,
+          deadline,
+          source: 'zap-docs',
+        },
+      })
+      return response.data
+    },
+  })
+
+export const useZapperAddLiquidity = () => {
+  const t = useTranslations()
+  const { startTxn, writeTxn, endTxn, sendTxn, updateTxn } = useTxn()
+
+  const [pending, setPending] = useState(false)
+  const { account, chainId } = useWallet()
+
+  const handleAddLiquidity = useCallback(
+    async ({ token, amount, mintInfo, route, deadline = 1800000000, isFarming = false }) => {
+      try {
+        if (!account) {
+          throw new Error('Please connect your wallet')
+        }
+
+        const key = uuidv4()
+        const approveId = uuidv4()
+        const addLiquidityId = uuidv4()
+        const approveNft = uuidv4()
+        const stakeId = uuidv4()
+
+        const transactions = {}
+
+        if (token.address !== 'BNB') {
+          transactions[approveId] = {
+            desc: 'Approving token',
+            status: TXN_STATUS.START,
+            hash: null,
+          }
+        }
+
+        transactions[addLiquidityId] = {
+          desc: t(mintInfo.noLiquidity ? 'Create pool and add liquidity' : 'Add Liquidity'),
+          status: TXN_STATUS.START,
+          hash: null,
+        }
+
+        if (isFarming) {
+          transactions[approveNft] = {
+            desc: `${t('Approve')} LP`,
+            status: TXN_STATUS.START,
+            hash: null,
+          }
+
+          transactions[stakeId] = {
+            desc: `${t('Stake')} LP`,
+            status: TXN_STATUS.START,
+            hash: null,
+          }
+        }
+
+        startTxn({ key, transactions, title: t('Add Liquidity') })
+        setPending(true)
+
+        let buildData = {
+          routerAddress: null,
+          callData: null,
+          value: null,
+        }
+
+        try {
+          const response = await axios.post(
+            `${BASE_ZAPPER_URL}/in/route/build`,
+            {
+              sender: getAddress(account),
+              route,
+              deadline,
+              source: 'zap-docs',
+            },
+            {
+              headers: {
+                'x-client-id': 'zap-docs',
+              },
+            },
+          )
+
+          buildData = response.data.data
+        } catch (error) {
+          return
+        }
+
+        let isApproved = false
+        const tokenContract = getERC20Contract(token.address, chainId)
+        if (token.address !== 'BNB') {
+          const allowance = await readCall(tokenContract, 'allowance', [account, buildData.routerAddress])
+          isApproved = fromWei(allowance, token.decimals).gte(amount)
+        }
+
+        // MARK: APPROVE TOKENS
+        setPending(true)
+        if (!isApproved) {
+          if (!(await writeTxn(key, approveId, tokenContract, 'approve', [buildData.routerAddress, maxUint256]))) {
+            setPending(false)
+            return
+          }
+        } else {
+          updateTxn({ key, approveId, status: TXN_STATUS.SUCCESS, hash: '' })
+        }
+
+        // MARK: ADD LIQUIDITY
+        const hash = sendTxn(key, addLiquidityId, buildData.routerAddress, buildData.callData, buildData.value)
+        const addTxRecieve = await waitCall(hash)
+
+        if (isFarming) {
+          const farmingCenter = getFarmingCenterContract(chainId)
+          const incentiveMaker = getInsentiveContract(chainId)
+          const positionManger = getPositionManagerContract(chainId, 3)
+
+          // MARK: APPROVE LP TOKEN FOR FARMING
+          const decodeData = NonfungiblePositionManager.getMintedPosition(addTxRecieve, chainId)
+          const nftId = decodeData.args?.tokenId
+          const poolAddress = decodeData.args?.pool
+
+          if (
+            !(await writeTxn(key, approveNft, positionManger, 'approveForFarming', [
+              nftId,
+              true,
+              farmingCenter.address,
+            ]))
+          ) {
+            setPending(false)
+            return
+          }
+
+          // MARK: STAKE LP TOKEN FOR FARMING
+          const poolKey = await readCall(incentiveMaker, 'poolToKey', [poolAddress], chainId)
+          if (!(await writeTxn(key, stakeId, farmingCenter, 'enterFarming', [poolKey, nftId]))) {
+            setPending(false)
+            return
+          }
+        }
+
+        endTxn({ key, final: 'Liquidity Add Successful' })
+        setPending(false)
+      } catch (e) {
+        setPending(false)
+        throw e
+      }
+    },
+    [account, t, startTxn, chainId, sendTxn, endTxn, writeTxn, updateTxn],
+  )
+
+  return { handleAddLiquidity, pending }
+}
