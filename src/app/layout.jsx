32e19ddfc81d646d088f62c8d11927ee3303e2f6
HieuPM <hieu.pm@zinza.com.vn>
2025-02-10 06:37:44 +0000

    fix: update MetaCRM tracking script version

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -63,7 +63,7 @@ export default function RootLayout({ children }) {
     <html lang='en'>
       <head>
         <script
-          src='https://attribution.metacrm.inc/tracking-1-2-0.js'
+          src='https://attribution.metacrm.inc/tracking-1-2-1.js'
           id='metacrm-tracking'
           crossOrigin='anonymous'
           {...metaCrmConfig}
