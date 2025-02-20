99a31b4a41ffc6bcd2f4049acb39c53216704b22
HieuPM <hieupm@thena.fi>
2025-02-20 21:06:30 +0700

    fix: get quote for BAD

--- src/hooks/fusion/useThenaQuote.js
+++ src/hooks/fusion/useThenaQuote.js
@@ -2,11 +2,11 @@ import BigNumber from 'bignumber.js'
 import { WBNB } from 'thena-sdk-core'
 import { useSimulateContract } from 'wagmi'
 
-import { getFusionQuoterContract } from '@/lib/contracts'
+import { getRouterContract } from '@/lib/contracts'
 import { toWei } from '@/lib/utils'
 
 export const useThenaQuote = (fromAsset, toAsset, fromAmount, networkId, enabled) => {
-  const quoterContract = getFusionQuoterContract(networkId)
+  const ThenaRouterV2Contract = getRouterContract(networkId)
 
   const amount = toWei(
     new BigNumber(fromAmount).decimalPlaces(fromAsset?.decimals ?? 18, BigNumber.ROUND_DOWN).toString(),
@@ -17,9 +17,9 @@ export const useThenaQuote = (fromAsset, toAsset, fromAmount, networkId, enabled
   const token1Address = toAsset?.address === 'BNB' ? WBNB[networkId].address : toAsset?.address
 
   return useSimulateContract({
-    ...quoterContract,
-    functionName: 'quoteExactInputSingle',
-    args: [token0Address, token1Address, amount, 0n],
+    ...ThenaRouterV2Contract,
+    functionName: 'getAmountOut',
+    args: [amount, token0Address, token1Address],
     query: {
       enabled,
     },
