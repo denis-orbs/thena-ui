92163f1e1dbdf13264c2cbb37141c941ec2bc8dd
namnh <nam.nh1@zinza.com.vn>
2025-03-11 17:04:48 +0700

    fix: zapper warning message

--- src/components/common/AddLiquidity/components/WarningZapper.jsx
+++ src/components/common/AddLiquidity/components/WarningZapper.jsx
@@ -12,46 +12,51 @@ function WarningZapper() {
 
   return (
     <Box
-      className={cn('flex flex-row items-start gap-2.5 border border-primary-800 bg-primary-950 py-4 md:gap-4 lg:py-8')}
+      className={cn('flex flex-row items-start gap-2.5 border border-primary-800 bg-primary-950 p-4 md:gap-4 lg:p-8')}
     >
-      <InfoIcon className='w-5 min-w-5 stroke-primary-600 md:w-8 md:min-w-8' />
-      <div>
-        <Paragraph className='mt-0.5 text-sm font-medium text-primary-100 md:text-xl'>
-          Important Information about Zapper
-        </Paragraph>
+      <InfoIcon className='my-1 w-5 min-w-5 stroke-primary-600 md:w-8 md:min-w-8' />
+      <div className=''>
+        <div className='flex items-start justify-between md:items-center'>
+          <Paragraph className='text-base font-medium text-primary-100 md:text-xl'>
+            Important Information about Zapper
+          </Paragraph>
+
+          <ChevronUpIcon
+            className={cn(
+              'w-7 min-w-7 cursor-pointer p-1 transition-all duration-300 ease-in-out md:w-9 md:min-w-9 md:p-2',
+              !showWarning && 'rotate-180',
+            )}
+            onClick={() => setShowWarning(show => !show)}
+          />
+        </div>
+
         <motion.div
           initial={{ opacity: 0, y: 0, height: 0 }}
           animate={showWarning ? { opacity: 1, y: 0, height: 'auto' } : { opacity: 0, y: 0, height: 0 }}
           transition={{ duration: 0.3, ease: 'easeInOut' }}
-          className='mt-2 overflow-hidden'
+          className={cn('overflow-hidden', showWarning && 'mt-2 md:mt-4')}
         >
-          <TextSubHeading className='space-y-2 text-sm text-primary-100 md:text-base'>
-            <Paragraph>This feature is incompatible with tokens that have tax implications.</Paragraph>
-            <Paragraph className='mb-2'>
+          <TextSubHeading className='flex flex-col text-sm md:text-base'>
+            <Paragraph className='text-primary-100'>
+              This feature is incompatible with tokens that have tax implications.
+            </Paragraph>
+            <Paragraph className='text-primary-100'>
               If you are zapping a considerable amount of funds, please ensure to use protection against sandwich
               attacks to safeguard your investment. This precaution helps protecting your transaction from potential
               front-running and other malicious activities.
             </Paragraph>
             <Link
               target='_blank'
-              className='flex items-center gap-2 text-primary-600'
+              className='mt-2 flex items-start gap-2 text-primary-600 md:mt-4 md:items-center'
               href='https://www.bnbchain.org/en/blog/protecting-users-from-sandwich-attacks-bnb-chain-introduces-mev-protection-with-several-wallets'
               rel='noreferrer'
             >
               Learn more about protection from sandwich attacks here
-              <LinkExternalPrimaryIcon className='inline-block h-4 w-4 !stroke-primary-600' />
+              <LinkExternalPrimaryIcon className='inline-block w-4 min-w-4 !stroke-primary-600' />
             </Link>
           </TextSubHeading>
         </motion.div>
       </div>
-
-      <ChevronUpIcon
-        className={cn(
-          'w-7 min-w-7 cursor-pointer p-1 transition-all duration-300 ease-in-out md:w-9 md:min-w-9 md:p-2',
-          !showWarning && 'rotate-180',
-        )}
-        onClick={() => setShowWarning(show => !show)}
-      />
     </Box>
   )
 }
