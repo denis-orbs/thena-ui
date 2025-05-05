98ccc4707e09a643a95065dff07bc690d9b2fe2e
HieuPM <hieupm@thena.fi>
2025-05-05 11:59:39 +0700

    fix: my profile

--- src/modules/Dashboard/Profile/index.jsx
+++ src/modules/Dashboard/Profile/index.jsx
@@ -4,22 +4,23 @@ import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useTranslations } from 'next-intl'
 import Avatar from 'public/images/home/stats/socials/social-1.png'
-import React, { useEffect, useMemo, useRef, useState } from 'react'
+import React, { useMemo } from 'react'
 import useSWR from 'swr'
 
 import { fetchAchievements } from '@/app/arena/profile/UserCompletedAchievements'
-import { NeutralBadge } from '@/components/badges/Badge'
 import Box from '@/components/box'
 import { EmphasisButton, PrimaryButton } from '@/components/buttons/Button'
+import { EmphasisIconButton } from '@/components/buttons/IconButton'
 import NextImage from '@/components/image/NextImage'
 import Skeleton from '@/components/skeleton'
 import { NewTextHeading, NewTextSubHeading, Paragraph, TextHeading } from '@/components/typography'
 import { fetchUserInfo } from '@/context/userInfoContext'
 import { useVeTHEsContext } from '@/context/veTHEsContext'
+import { fetchFollower, fetchFollowing } from '@/hooks/useUserFollow'
 import useWallet from '@/hooks/useWallet'
-import { useWindowSize } from '@/hooks/useWindowSize'
 import { cn, formatAddress, formatAmount, sortAchievements } from '@/lib/utils'
 import AchievementBasicIcon from '@/modules/Achievements/AchievementBasicIcon'
+import { PenEditIcon } from '@/svgs'
 
 import SectionDivider from '../SectionDivider'
 
@@ -49,48 +50,29 @@ function DashboardProfile() {
     [veTHEs],
   )
 
-  const achievementsRef = useRef(null)
-  const [visibleCount, setVisibleCount] = useState()
+  const { data: following } = useSWR(['following', account], () => fetchFollowing(account.toLowerCase()), {
+    refreshInterval: 60000,
+  })
 
-  useEffect(() => {
-    if (sortedData.length === 0) return
-    setVisibleCount(prev => {
-      if (!prev || prev === 0) return sortedData.length
-      return prev
-    })
-  }, [sortedData.length])
+  const { data: followers } = useSWR(['followers', account], () => fetchFollower(account.toLowerCase()), {
+    refreshInterval: 60000,
+  })
 
-  const windowSize = useWindowSize()
+  const followingCount = useMemo(() => following?.length ?? '-', [following?.length])
 
-  useEffect(() => {
-    const { width } = windowSize
-
-    if (width >= 834 && width < 1024) {
-      setVisibleCount(7)
-    } else if (width >= 1024 && width < 1280) {
-      setVisibleCount(9)
-    } else if (width >= 1280 && width < 1356) {
-      setVisibleCount(10)
-    } else if (width >= 1356 && width < 1536) {
-      setVisibleCount(12)
-    } else if (width >= 1536) {
-      setVisibleCount(13)
-    } else {
-      setVisibleCount(8)
-    }
-  }, [windowSize])
+  const followersCount = useMemo(() => followers?.length ?? '-', [followers?.length])
 
   return (
     <div
       className={cn(
-        'rounded-xl md:col-span-2',
+        'rounded-xl max-md:mb-16 md:col-span-2',
         (!userInfo || !userInfo.usernameNfts?.length) &&
           'bg-[url("/images/profile-bg.png")] bg-cover bg-center bg-no-repeat md:col-span-1',
       )}
     >
       <Box
         className={cn(
-          'flex h-full flex-col gap-1.5 !p-4 max-md:mt-4 md:gap-6',
+          'flex h-full flex-col gap-1.5 !p-0 md:gap-6',
           (!userInfo || !userInfo.usernameNfts?.length) && 'bg-transparent',
         )}
       >
@@ -128,88 +110,112 @@ function DashboardProfile() {
           </div>
         ) : (
           <>
-            <div className='hidden h-full flex-col justify-between md:flex' ref={achievementsRef}>
-              <div className='flex gap-4'>
-                <div className='size-[145px]'>
-                  <Image
-                    alt='avatar'
-                    src={userInfo?.avatar?.replace('ipfs.io', 'w3s.link') ?? Avatar}
-                    className='size-full rounded-[20px]'
-                    width={0}
-                    height={0}
-                  />
+            <div className='hidden h-full flex-col justify-between md:flex'>
+              <div className='flex h-full gap-4'>
+                <div
+                  className='relative h-full min-h-[432px] w-[27%] rounded-bl-xl rounded-tl-xl bg-cover bg-center bg-no-repeat'
+                  style={{
+                    backgroundImage: `url('${
+                      userInfo?.avatar?.replace('ipfs.io', 'w3s.link') || '/images/profile-bg.png'
+                    }')`,
+                  }}
+                >
+                  <NewTextHeading className='absolute top-0 w-full p-4 text-3xl md:text-3xl'>
+                    {t('My Profile')}
+                  </NewTextHeading>
+                  <Link href='/arena/profile'>
+                    <EmphasisButton className='absolute bottom-4 w-full'>{t('Manage')}</EmphasisButton>
+                  </Link>
                 </div>
 
-                <div className='flex flex-col gap-4 max-md:hidden'>
-                  <NewTextHeading className='text-xl !leading-6 text-neutral-500 md:text-xl'>
-                    {userInfo?.isVerified ? 'Verified' : 'Unverified'}
-                  </NewTextHeading>
-                  {userInfo?.username && (
-                    <NewTextHeading className='flex gap-2 text-xl !leading-6 md:text-xl'>
+                <div className='relative flex h-full w-[73%] flex-col gap-4 py-4 pr-4 max-md:hidden'>
+                  <div className='flex items-center justify-between gap-4'>
+                    <NewTextHeading className='flex gap-2 text-xl uppercase !leading-6 md:text-xl'>
                       <span className='text-neutral-500'>{t('Thena ID')}:</span>
-                      <span className='text-warn-600'>{userInfo?.username}</span>
+                      <span className='text-warn-600'>{userInfo?.username || formatAddress(account)}</span>
                     </NewTextHeading>
-                  )}
-                  <TextHeading className='flex gap-2 font-archia text-xl font-semibold !leading-6 lg:text-xl'>
-                    <span className='text-neutral-500'>{t('Wallet')}:</span>
+                    <Link href='/arena/profile/edit'>
+                      <EmphasisIconButton Icon={PenEditIcon} />
+                    </Link>
+                  </div>
+                  <TextHeading className='flex h-10 items-center gap-2 font-archia text-xl font-semibold !leading-6 lg:text-xl'>
+                    <NewTextHeading className='text-xl !leading-6 text-neutral-500 md:text-xl'>
+                      {userInfo?.isVerified ? 'Verified' : 'Unverified'}
+                    </NewTextHeading>
                     <span className='text-neutral-500'>{formatAddress(account)}</span>
                   </TextHeading>
+                  <div className='flex h-10 flex-wrap items-center gap-4'>
+                    <TextHeading className='font-archia text-xl font-semibold'>
+                      <span className='text-neutral-500'>{t(followersCount !== 1 ? 'Followers' : 'Follower')}</span>
+                      <span className='ml-2'>{followersCount}</span>
+                    </TextHeading>
+                    <TextHeading className='font-archia text-xl font-semibold'>
+                      <span className='text-neutral-500'>{t('Following')}</span>
+                      <span className='ml-2'>{followingCount}</span>
+                    </TextHeading>
+                  </div>
+                  <div className='flex min-h-10 flex-wrap items-center gap-8'>
+                    <TextHeading className='font-archia text-xl font-semibold lg:text-xl'>
+                      <span className='text-neutral-500'>{t('veTHE Power')}</span>
+                      <span className='ml-4'>{formatAmount(veTHEPower)}</span>
+                    </TextHeading>
+                    <TextHeading className='font-archia text-xl font-semibold lg:text-xl'>
+                      <span className='text-neutral-500'>{t('Thena ID´s')}</span>
+                      <span className='ml-4'>{userInfo?.usernameNfts?.length || 0}</span>
+                    </TextHeading>
+                    <TextHeading className='font-archia text-xl font-semibold lg:text-xl'>
+                      <span className='text-neutral-500'>{t('Rank')}</span>
+                      <span className='ml-4'>{userInfo?.rank}</span>
+                    </TextHeading>
+                  </div>
+                  <div
+                    className={cn(
+                      'flex flex-wrap items-center overflow-hidden',
+                      'h-[130px]', // 65px height * 2 rows + approx spacing
+                      sortedData.length > 0 && 'mt-2',
+                    )}
+                  >
+                    {sortedData.map(item => (
+                      <AchievementBasicIcon
+                        item={item}
+                        key={item.achievement.id}
+                        className='gap-0 p-0'
+                        classNames={{ item: 'h-[65px] w-[65px]' }}
+                      />
+                    ))}
+                  </div>
                   {userInfo?.xProfileUrl && (
-                    <div className='flex gap-2'>
+                    <div className='flex h-11 w-full items-center gap-2'>
                       <TextHeading className='font-archia text-xl font-semibold !leading-6 text-neutral-500 lg:text-xl'>
                         {t('Socials')}:{' '}
                       </TextHeading>
-                      <Link href={`https://x.com/${userInfo?.xProfileUrl}`} rel='nofollow noopener' target='_blank'>
-                        <NeutralBadge className='flex items-center lg:text-xs'>
-                          <NextImage alt='svg' className='mr-2 w-fit' src='/images/footer/x.svg' />
-                          <span className='line-clamp-1'>@{userInfo?.xProfileUrl}</span>
-                        </NeutralBadge>
+                      <Link
+                        href={`https://x.com/${userInfo?.xProfileUrl}`}
+                        rel='nofollow noopener size-8'
+                        target='_blank'
+                      >
+                        <div className='flex size-8 items-center justify-center rounded-md bg-neutral-700'>
+                          <NextImage alt='svg' className='size-4' src='/images/footer/x.svg' />
+                        </div>
                       </Link>
                     </div>
                   )}
                 </div>
               </div>
-
-              <div className='mt-6 flex flex-wrap gap-8'>
-                <TextHeading className='font-archia text-xl font-semibold lg:text-xl'>
-                  <span className='text-neutral-500'>{t('veTHE Power')}</span>
-                  <span className='ml-4'>{formatAmount(veTHEPower)}</span>
-                </TextHeading>
-                <TextHeading className='font-archia text-xl font-semibold lg:text-xl'>
-                  <span className='text-neutral-500'>{t('Thena ID´s')}</span>
-                  <span className='ml-4'>{userInfo?.usernameNfts?.length || 0}</span>
-                </TextHeading>
-                <TextHeading className='font-archia text-xl font-semibold lg:text-xl'>
-                  <span className='text-neutral-500'>{t('Rank')}</span>
-                  <span className='ml-4'>{userInfo?.rank}</span>
-                </TextHeading>
-              </div>
-
-              <div className={cn('mt-10 flex items-center')}>
-                {sortedData.slice(0, visibleCount).map(item => (
-                  <AchievementBasicIcon
-                    item={item}
-                    key={item.achievement.id}
-                    className='gap-0 p-0'
-                    classNames={{ item: 'h-[65px] w-[65px]' }}
-                  />
-                ))}
-              </div>
-
-              <EmphasisButton className='mt-6 w-full' onClick={() => push('/arena/profile')}>
-                {t('Manage')}
-              </EmphasisButton>
             </div>
 
-            <div className='flex flex-col gap-4 md:hidden'>
-              <TextHeading className='flex items-center gap-2 font-archia text-xl font-semibold'>
-                {userInfo?.username && <span className='text-warn-600'>{userInfo.username}</span>}
-                <span className='text-neutral-500'>{formatAddress(account)}</span>
-              </TextHeading>
+            <div className='flex flex-col gap-4 p-4 md:hidden'>
+              <div className='gap-1.5'>
+                <NewTextHeading className='text-xl'>{t('My Profile')}</NewTextHeading>
+                <TextHeading className='flex items-center gap-2 font-archia text-xl font-semibold'>
+                  {userInfo?.username && <span className='text-warn-600'>{userInfo.username}</span>}
+                  <span className='text-neutral-500'>{formatAddress(account)}</span>
+                </TextHeading>
+              </div>
               <Image
                 alt='avatar'
                 src={userInfo?.avatar?.replace('ipfs.io', 'w3s.link') ?? Avatar}
-                className='mx-auto rounded-[20px]'
+                className='mx-auto rounded-full'
                 width={124}
                 height={124}
               />
@@ -221,7 +227,7 @@ function DashboardProfile() {
         )}
       </Box>
 
-      <SectionDivider />
+      <SectionDivider className='mt-4' />
     </div>
   )
 }
