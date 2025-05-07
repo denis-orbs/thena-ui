7bc7d4dfd5a4cf061d5998a760e68283e98a79bf
hieupmzz <hieupm@thena.fi>
2025-05-07 17:51:39 +0700

    fix: go back button on add liquidity page

--- src/app/pools/(add-liquidity)/add-liquidity/page.jsx
+++ src/app/pools/(add-liquidity)/add-liquidity/page.jsx
@@ -1,9 +1,10 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 
 import LayoutWithBackButton from '@/components/common/LayoutWithBackButton'
+import { PAIR_TYPES } from '@/constant'
 
 import Step1 from './Step1'
 import Step2 from './Step2'
@@ -12,9 +13,34 @@ import Step3 from './Step3'
 export default function AddLiquidityPage() {
   const params = useSearchParams()
   const step = Number(params.get('step') ?? 1)
+  const pairType = params.get('pairType') || PAIR_TYPES.LSD
+  const firstAddress = params.get('firstAddress')
+  const secondAddress = params.get('secondAddress')
+
+  const [stepUrls, setStepUrls] = useState({
+    1: '/pools/add-liquidity?step=1',
+    2: '/pools/add-liquidity?step=2',
+  })
+
+  useEffect(() => {
+    if (firstAddress && secondAddress) {
+      setStepUrls({
+        1: `/pools/add-liquidity?step=1&pairType=${pairType}`,
+        // eslint-disable-next-line max-len
+        2: `/pools/add-liquidity?step=2&pairType=${pairType}&firstAddress=${firstAddress}&secondAddress=${secondAddress}`,
+      })
+    }
+  }, [firstAddress, secondAddress, pairType])
+
+  const backUrl = useMemo(() => {
+    if (step === 1) return '/pools'
+    if (step === 2) return stepUrls[1]
+    if (step === 3) return stepUrls[2]
+    return '/pools'
+  }, [step, stepUrls])
 
   return (
-    <LayoutWithBackButton>
+    <LayoutWithBackButton backUrl={backUrl}>
       <div className='container mx-auto flex flex-col'>
         {step === 1 && <Step1 />}
         {step === 2 && <Step2 />}
--- src/components/common/LayoutWithBackButton.jsx
+++ src/components/common/LayoutWithBackButton.jsx
@@ -6,9 +6,9 @@ import { ArrowLeftIcon } from '@/svgs'
 
 import { TextButton } from '../buttons/Button'
 
-function LayoutWithBackButton({ children, className }) {
+function LayoutWithBackButton({ children, className, backUrl }) {
   const t = useTranslations()
-  const { back } = useRouter()
+  const { back, push } = useRouter()
 
   return (
     <div className='mt-[64px] lg:mt-[92px]'>
@@ -16,7 +16,13 @@ function LayoutWithBackButton({ children, className }) {
         <TextButton
           className='ml-4 w-fit outline-0 max-lg:pl-0 lg:ml-10'
           LeadingIcon={ArrowLeftIcon}
-          onClick={() => back()}
+          onClick={() => {
+            if (backUrl) {
+              push(backUrl)
+            } else {
+              back()
+            }
+          }}
         >
           {t('Back')}
         </TextButton>
