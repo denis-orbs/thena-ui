75c11d8f0b5200dc22997e9c48fb188f169d5b83
denis-orbs <denis@orbs.com>
2025-05-28 07:53:55 +0300

    ui fix

--- src/modules/TwapAndLimit/index.jsx
+++ src/modules/TwapAndLimit/index.jsx
@@ -102,10 +102,14 @@ function LimitPrice() {
   return (
     <div className='twap-limit-panel'>
       <div className='twap-limit-panel-header'>
-        <p>When 1</p>
-        <CircleImage src={tokens.topToken?.logoUrl} alt='token logo' width={24} height={24} />
-        <p> {tokens.topToken?.symbol} is worth</p>
+        <div className='twap-limit-panel-header-left'>
+          <p>When 1</p>
+          <CircleImage src={tokens.topToken?.logoUrl} alt='token logo' width={24} height={24} />
+          <p> {tokens.topToken?.symbol} is worth</p>
+        </div>
+        <TextIconButton onClick={onInvert} className='twap-limit-panel-invert-button' Icon={SwitchHorizontalV2Icon} />
       </div>
+
       <div className='twap-limit-panel-input-container'>
         <TokenInput
           className='twap-limit-panel-input'
@@ -119,7 +123,6 @@ function LimitPrice() {
           autoFocus
         />
         <Widget.LimitPrice.PercentSelector />
-        <TextIconButton onClick={onInvert} className='twap-limit-panel-invert-button' Icon={SwitchHorizontalV2Icon} />
       </div>
     </div>
   )
