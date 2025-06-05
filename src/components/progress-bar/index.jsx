04d72b569544bf9a66856221980d7ee4702b8aed
hieupmzz <hieupm@thena.fi>
2025-06-05 10:47:23 +0700

    fix: vote progress bar

--- src/components/progress-bar/index.jsx
+++ src/components/progress-bar/index.jsx
@@ -8,15 +8,18 @@ export function ProgressBar({ progress, suffix }) {
   return (
     <div
       className={cn(
-        'flex h-6 w-full items-center justify-between overflow-hidden',
+        'relative flex h-6 w-full items-center justify-between overflow-hidden',
         'rounded-full border border-neutral-600 bg-neutral-800',
       )}
     >
       <div
-        className={cn('h-full border-r-[3px] border-primary-300 transition-all ease-in-out', 'bg-progress-gradient')}
+        className={cn(
+          'absolute h-full border-r-[3px] border-primary-300 transition-all ease-in-out',
+          'bg-progress-gradient',
+        )}
         style={{ width: `${progress}%` }}
       />
-      {suffix && <Paragraph className='mr-4 lg:text-sm'>{suffix}</Paragraph>}
+      {suffix && <Paragraph className='z-10 mr-4 w-full text-end lg:text-sm'>{suffix}</Paragraph>}
     </div>
   )
 }
