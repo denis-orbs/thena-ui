73f4c3a158f8d4bd30acb3aae49bb4b13f9d51c4
thenaursa <defihyper88@gmail.com>
2025-10-09 13:02:36 +0200

    chore: update headers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const cspHeader = `
-          frame-src 'self' https://stakeridoo.github.io; // Allow iframes from this source
-        `
 const nextConfig = {
   eslint: {
     dirs: ['src'],
@@ -136,11 +133,12 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
         headers: [
           {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ''),
+            key: 'X-Frame-Options',
+            value: 'ALLOW-FROM https://stakeridoo.github.io',
           },
         ],
       },
