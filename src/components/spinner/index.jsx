cea1f9522bd534bc841f4b1c0975685f44dc945c
thenaursa <defihyper88@gmail.com>
2025-11-05 14:07:23 -0700

    chore: fix spinner

--- src/components/spinner/index.jsx
+++ src/components/spinner/index.jsx
@@ -1,18 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
 import { cn } from '@/lib/utils'
 
-import NextImage from '../image/NextImage'
-
 function Spinner({ className }) {
   return (
-    <NextImage
-      className={cn('animate-spin', className)}
-      width={20}
-      height={20}
-      src='/images/spin.png'
-      alt='thena spin'
-    />
+    <Image className={cn('animate-spin', className)} width={20} height={20} src='/images/spin.png' alt='thena spin' />
   )
 }
 
