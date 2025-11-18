90589ea083fb524409ec2528732796418dbb8dc0
hieupmzz <hieupm@thena.fi>
2025-11-18 13:25:13 +0700

    chore: remove banner migrage

--- src/app/content-studio/layout.jsx
+++ src/app/content-studio/layout.jsx
@@ -2,25 +2,14 @@
 
 import StudioShell from '@/modules/Studio/StudioLayout/StudioShell'
 import Tabs from '@/modules/Studio/StudioLayout/Tabs'
-import { useMigratePositionWarning } from '@/state/positions/hooks'
-import cn from '@/utils/classes'
 
 export default function ContentStudioLayout({ children }) {
-  const { showBannerMigrate } = useMigratePositionWarning()
-
   return (
     <div>
-      <div
-        className={cn('fixed top-[70px] z-40 w-full md:top-[90px]', showBannerMigrate && 'top-[186px] md:top-[144px]')}
-      >
+      <div className='fixed top-[70px] z-40 w-full md:top-[90px]'>
         <Tabs />
       </div>
-      <div
-        className={cn(
-          'mt-[158px] flex w-full overflow-x-auto md:mt-[206px]',
-          showBannerMigrate && 'mt-[274px] md:mt-[260px]',
-        )}
-      >
+      <div className='mt-[158px] flex w-full overflow-x-auto md:mt-[206px]'>
         <StudioShell>{children}</StudioShell>
       </div>
     </div>
--- src/components/common/LayoutWithBackButton.jsx
+++ src/components/common/LayoutWithBackButton.jsx
@@ -1,20 +1,10 @@
-import { useMigratePositionWarning } from '@/state/positions/hooks'
 import cn from '@/utils/classes'
 
 import BackButton from '../buttons/BackButton'
 
 function LayoutWithBackButton({ children, className, backUrl, hiddenBackButton }) {
-  const { showBannerMigrate } = useMigratePositionWarning()
-
   return (
-    <div
-      className={cn(
-        'flex flex-col',
-        showBannerMigrate && 'mt-2 max-md:mt-[72px]',
-        !showBannerMigrate && 'mt-[72px] mb-2 lg:mt-[100px]',
-        hiddenBackButton && 'lg:mt-[92px]',
-      )}
-    >
+    <div className={cn('mt-[72px] mb-2 flex flex-col lg:mt-[100px]', hiddenBackButton && 'lg:mt-[92px]')}>
       {!hiddenBackButton && (
         <div className='ml-4 hidden max-xl:pl-0 xl:ml-10 xl:block'>
           <BackButton href={backUrl} />
